708444d1cf27aa400261345d3703841ebfd9132a
Kvasir-reborn <123034787+Kvasir-reborn@users.noreply.github.com>
2023-08-09 14:57:02 -0500

    Add typing hints, clarify docstrings.

--- date_detection.py
+++ date_detection.py
@@ -10,20 +10,23 @@ logging.basicConfig(
     filename="logging.txt",
     format="%(asctime)s -  %(levelname)s -  %(message)s",
 )
-logging.disable(logging.CRITICAL)  # Note out to enable logging.
+# logging.disable(logging.CRITICAL)  # Note out to enable logging.
 
 text_block = """
-The regular expression doesn’t have to detect correct days for each month or for leap years; it will accept nonexistent dates like 31/02/2020 or 31/04/2021. 31/06/2100. 
+The regular expression doesn’t have to detect correct days for each month or for leap years; it will 
+accept nonexistent dates like 31/02/2020 or 31/04/2021. 31/06/2100. 
 (01/01/2100.)
 32/01/1234 ain't even real, man.
-02/13/1000*(^
-01/01/3214—
-**&&^02/02/1988
-Write a function 29/02/1800 that uses regular expressions 29/02/2004 to make sure the password string it is passed is strong. A strong password is defined as 29/02/2000 one that is at least eight characters long, contains both uppercase and lowercase characters, and has at least 29/02/1900 one digit. You may 29/02/2500 need to test the 29/02/2500 string against multiple 30/04/2001 regex patterns to validate its strength 31/04/2001.
+02/13/1000*(^01/01/3214—**&&^02/02/1988
+Write a function 29/02/1800 that uses regular expressions 29/02/2004 to make sure the password string 
+it is passed is strong. A strong password is defined as 29/02/2000 one that is at least eight characters long, 
+contains both uppercase and lowercase characters, and has at least 29/02/1900 one digit. You may 29/02/2500 
+need to test the 29/02/2500 string against multiple 30/04/2001 regex patterns to validate its strength 
+31/04/2001.
 """
 
 
-def main():
+def main() -> None:
     """Print valid dates to console."""
     matches = create_regex(text_block)
     valid_dates = vali_date(matches)
@@ -32,15 +35,16 @@ def main():
         print(f"— {date}")
 
 
-def create_regex(user_input):
-    """Make regex for DD/MM/YYYY date format and return match object."""
+def create_regex(text: str) -> list[tuple]:
+    """Make regex for DD/MM/YYYY date format and return list of tuples."""
     date_regex = re.compile(r"(\d{2})/(\d{2})/(\d{4})")
-    match_obj = date_regex.findall(user_input)
+    match_obj = date_regex.findall(text)
+    logging.debug(match_obj)
     return match_obj
 
 
-def vali_date(matches):
-    """Unpack match object, validate elements and then return list of valid dates."""
+def vali_date(matches: list[tuple]) -> list[str]:
+    """Unpack tuples from list, perform validation and return list of strings containing valid dates."""
     matching_dates = []
     for index, _ in enumerate(matches):
         day, month, year = matches[index]
